59f84be60b4d755de1763fbe4a0b152491a37e4c
nell <autoprojetmk@gmail.com>
2026-07-29 13:17:27 +0100

    Nouveau lien Signal

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
             <a href="https://www.tiktok.com/@deustchlandautocar1" target="_blank" rel="noopener noreferrer" title="TikTok">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48Z"/></svg>
             </a>
-            <a href="https://signal.me/#eu/WSXlnFmOYijUqV5fXOh56rNX7AGVCgjmnhjE0xv6SHIiuAKxcp5Gcplaa8QUIgS7" target="_blank" rel="noopener noreferrer" title="Signal">
+            <a href="https://signal.me/#eu/XOOLpKZMXvAdh4IUQ8EyLHATucwsJZa28mxn6YXd7LgxoFbj5v5B3bY7QIQ_0umM" target="_blank" rel="noopener noreferrer" title="Signal">
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
             </a>
           </div>
